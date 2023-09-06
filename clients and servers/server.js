14994024a74a8bb68098c877e959db3600cfec91
Russell <flahertypro1@outlook.com>
2023-09-06 20:53:19 +0100

    added views directory and html file

--- clients and servers/server.js
+++ clients and servers/server.js
@@ -1,13 +1,21 @@
 const http = require('http'); 
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method );
     // set header type 
     res.setHeader('Content-Type','text/plain' );
-
-    res.write('<p> Hello Ninjas </p> ');
-    res.write('<p> Hello Ninjas  </p> ');
-    res.end();
+    // send a html file 
+    fs.readFile('./views/index.html', (err, data) => {
+        if(err){
+            console.log(err); 
+            res.end();
+        }
+        else{
+          //  res.write(data); 
+            res.end(data);
+        }
+    });
 });
 
 server.listen(3000, 'localhost' , ()   => {
