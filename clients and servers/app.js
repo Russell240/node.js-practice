f8177261d91c0790324b86ee1cf852f601eb27e1
Russell <flahertypro1@outlook.com>
2023-11-09 21:17:34 +0000

    added to app.js

--- clients and servers/app.js
+++ clients and servers/app.js
@@ -2,10 +2,27 @@ const express= require('express');
 
 const app = express();
 //listen for request 
+
+// register view engine 
+app.set('view engine', 'ejs')
+app.set('views', )
+
 app.listen(3000); 
 
 app.get('/about');
 
 app.get('/', (req, res) => {
-    res.send('<p> home page </p>  ')
+   // res.send('<p> home page </p>  ')
+    res.render('index');
+
+});
+
+app.get('/about',(req, res) => {
+    res.render('about');
+});
+
+// error 404 
+
+app.use((req, res) => {
+    res.render('404')
 });
