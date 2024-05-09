0ee7cd489b86f3dad7698094684c0b99797a0926
Russell <flahertypro1@outlook.com>
2024-05-09 12:12:39 +0100

    app.listen added back to app.js

--- clients and servers/app.js
+++ clients and servers/app.js
@@ -1,18 +1,16 @@
 const express= require('express'); 
 const morgan= require('morgan');
 const mongoose= require('mongoose');
-const blog=  require('./models ');
+const Blog=  require('./models/blog');
 
-var app = express();
+var app = express();  
 const dbURI= 'mongodb+srv://netninja:!test1234@nodetuts.itn9mzv.mongodb.net/?retryWrites=true&w=majority&appName=nodetuts'; 
-mongoose.connect(dbURI, {useNewUrlParser:true})
-.then((result) => console.log('Connected to the Database') )
-.catch((err) => console.log(err)); 
+mongoose.connect(dbURI)
+.then((result) => console.log('Connected to the Database'), app.listen(3000))
+.catch((err) => console.log(err));  
 
 // register view engine 
 app.set('view engine', 'ejs')
-
-
 
 
 // middleware and static files 
@@ -22,8 +20,30 @@ app.use(morgan('dev'));
 // mongoose and mongo sandbox routes 
 
 app.get('/add-blog', (  req, res ) => {
-    const blog= new Blog( ); 
+    const blog= new Blog({
+        title:'new blog', 
+        snippet: 'about my new blog', 
+        body:'more about my new blog ' 
+    })
+
+    blog.save()
+        .then((result) => {
+           res.send(result);        
+        })
+        .catch(err => {
+            console.log(err);
+          });
 }  ); 
+
+app.get('/all-blogs',  (req, res) => {
+    Blog.find()
+     .then((result =>  {
+        res.rend(result)
+     })
+     .catch((err) => {
+        console.log(err);
+     }))
+} )
 
 app.get('/', (req, res) => {
    const blogs =[
